5abc55add371406eb5e246330601627f7156736f
aintsashqa <ujeromi@gmail.com>
2022-08-21 14:47:02 +0300

    fix: init schema migration column description

--- migrations/0001_init_schema.up.sql
+++ migrations/0001_init_schema.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS public.todo (
     id CHAR(36) NOT NULL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description TEXT(500)
+    description TEXT,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
     deleted_at TIMESTAMPTZ,
